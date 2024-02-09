2f841563fa87d7555259c381abaeeadbcb5dc757
Om Anand <omishere21@gmail.com>
2024-02-09 14:07:57 +0530

    VGA Text Buffer implementation

--- mini_os/src/main.rs
+++ mini_os/src/main.rs
@@ -1,19 +1,20 @@
 #![no_std]
 #![no_main]
+mod vga_buffer;
 use core::panic::PanicInfo;
-static HELLO: &[u8] = b"Hello World!";
+// static HELLO: &[u8] = b"Hello World!";
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-    loop {}
+    // vga_buffer::write_something();
+    // use core::fmt::Write;
+    // vga_buffer::WRITER.lock().write_str("Hello world").unwrap();
+    // write!(vga_buffer::WRITER.lock(), "some numbers: {} {}", 10, 3.14).unwrap();
+    println!("hello world{}", "!");
+    panic!("Some panic message!");
+    // loop {}
 }
