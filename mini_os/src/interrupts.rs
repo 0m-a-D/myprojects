2811d4a8bfa5b579cc8f6dfa2556fe927f673e18
Om Anand <omishere21@gmail.com>
2024-02-22 23:38:11 +0530

    Introduction to Paging
    add page fault handler

--- mini_os/src/interrupts.rs
+++ mini_os/src/interrupts.rs
@@ -1,11 +1,13 @@
 use crate::gdt;
 use crate::{print, println};
 use lazy_static::lazy_static;
+// lazily initializes a static variable when referenced for the first time
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
+        // breakpoint handler
         idt.breakpoint.set_handler_fn(breakpoint_handler);
         unsafe {
         idt.double_fault.set_handler_fn(double_fault_handler).set_stack_index(gdt::DOUBLE_FAULT_INDEX);
@@ -21,6 +23,9 @@ lazy_static! {
         // CPU handles interrupts the same way as exceptions.
         idt[InterrupIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
         }
+
+        //page fault handler
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt
     };
 }
@@ -29,6 +34,7 @@ pub fn init_idt() {
     IDT.load();
 }
 
+/// CPU EXCEPTION HANDLERS
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:?}", stack_frame);
 }
@@ -36,8 +42,58 @@ extern "x86-interrupt" fn double_fault_handler(
     stack_frame: InterruptStackFrame,
     _error_code: u64,
 ) -> ! {
-    panic!("EXCEPTION: DOUBLE FAULT\n{:?}", stack_frame);
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
+
+use crate::hlt_loop;
+use x86_64::structures::idt::PageFaultErrorCode;
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+    // CR2 register is set by CPU on page fault and it holds the virtual address that caused the
+    // page fault...
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
+}
+
+#[test_case]
+fn test_breakpoint_exception() {
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+}
+
+/// HARDWARE INTERRUPTS SECTION
+use pic8259::ChainedPics;
+use spin;
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterrupIndex {
+    Timer = PIC_1_OFFSET, // using C-like enum
+    Keyboard, // we didn't have to specify value 33, because it defaults to previous value + 1.
+}
+impl InterrupIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
+}
+
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
     print!(".");
 
@@ -72,36 +128,5 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStac
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InterrupIndex::Keyboard.as_u8());
-    }
-}
-
-#[test_case]
-fn test_breakpoint_exception() {
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
-}
-
-/// HARDWARE INTERRUPTS SECTION
-use pic8259::ChainedPics;
-use spin;
-
-pub const PIC_1_OFFSET: u8 = 32;
-pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
-
-pub static PICS: spin::Mutex<ChainedPics> =
-    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
-
-#[derive(Debug, Clone, Copy)]
-#[repr(u8)]
-pub enum InterrupIndex {
-    Timer = PIC_1_OFFSET, // using C-like enum
-    Keyboard, // we didn't have to specify value 33, because it defaults to previous value + 1.
-}
-impl InterrupIndex {
-    fn as_u8(self) -> u8 {
-        self as u8
-    }
-    fn as_usize(self) -> usize {
-        usize::from(self.as_u8())
     }
 }
--- mini_os/src/main.rs
+++ mini_os/src/main.rs
@@ -27,6 +27,28 @@ pub extern "C" fn _start() -> ! {
 
     // x86_64::instructions::interrupts::int3();
 
+    // creating a pagefault //
+    let ptr = 0x20555a as *mut u8;
+    unsafe {
+        let _x = *ptr;
+    }
+    println!("READ WORKED");
+    // 0x20555a is the address that points to a code page. Code pages are mapped read-only by
+    // bootloader... so we can read the data that this address points to, but can't write to it...
+
+    unsafe {
+        *ptr = 42;
+    }
+    println!("WRITE WORKED");
+
+    // accessing page tables
+    use x86_64::registers::control::Cr3;
+    let (level_4_page_table, _) = Cr3::read();
+    println!(
+        "LEVEL 4 PAGE TABLE AT: {:?}",
+        level_4_page_table.start_address()
+    ); // comment line 39 -> 42 to see the address of level 4 page table.
+
     #[cfg(test)] // using "cfg(test)" for conditional compiling...
     test_main(); // name of the test framework entry function
 
